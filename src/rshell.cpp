85be1f4ad57ffcdf62d0b0c26db31a4d7d621688
Lucas Xu <lxu003@ucr.edu>
2014-10-23 11:49:39 -0700

    fixed bugs

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -4,112 +4,180 @@
 #include <vector>
 #include <stdio.h> //perror
 #include <stdlib.h> //NULL, exit
-#include <string.h> //memcpy, strcmp
+#include <string.h> //strcpy, strcmp
 #include <sys/wait.h> //waitpid
 #include <unistd.h> //fork, execvp
 using namespace std;
 
-#define MAX_LENGTH 256 //for login and host name
-#define CONNECTOR_ERROR_MESSAGE \
-	"syntax error: connector must be preceded by a command"
-#define QUOTES_ERROR_MESSAGE \
-	"syntax error: unmatched quotes"
+#define MAX_LENGTH 256
 
-//separate special characters with whitespace for tokenization
-int preprocess(string& line, vector<string>& quoted) {
-	string processed = "";
-	for (size_t i = 0; i < line.size(); ++i) {
+void more_input(string& line) {
+	cout << "> ";
+	string next_line;
+	if (cin.eof())
+		exit(1);
+	getline(cin, next_line);
+	line += "\n" + next_line;
+}
+
+void parse_backslash(string& line,
+					int& i,
+					string& token) {
+	if (i+1 < line.size()) {
+		token += line[i+1];
+		i += 2;
+	}
+	else {
+		more_input(line);
+		i += 1;
+		while (i < line.size() && line[i] <= 32) ++i;
+	}
+}
+
+void parse_quotes(string& line,
+				 vector< vector<string> >& parsed,
+				 int& i,
+				 string& token) {
+	char delimiter = line[i];
+	++i;
+LOOP:
+	while (i < line.size() && line[i] != delimiter)
+		token += line[i++];
+	if (i == line.size()) {
+		more_input(line);
+		goto LOOP;
+	}
+	++i;
+}
+
+int parse_connector1(const string& line,
+					 vector< vector<string> >& parsed,
+					 int& i,
+					 int& j,
+					 string& token) {
+	if (token.size() == 0) {
+		if (parsed[j].size() == 0 ||
+				(j > 0 && parsed[j].size() == 1)) {
+			cerr << "syntax error: token '" << line[i] << '\'' << endl;
+			return 1;
+		}
+		token += line[i++];
+		parsed.push_back(vector<string>());
+		++j;
+	}
+	else {
+		parsed[j].push_back(token);
+		token =  "";
+		token += line[i++];
+		parsed.push_back(vector<string>());
+		++j;
+	}
+	return 0;
+}
+
+int parse_connector2(string& line,
+					 vector< vector<string> >& parsed,
+					 int& i,
+					 int& j,
+					 string& token) {
+	if (token.size() == 0) {
+		if (parsed[j].size() == 0 ||
+				(j > 0 && parsed[j].size() == 1)) {
+			cerr << "syntax error: token '" << line[i]
+				 << line[i+1] << '\'' << endl;
+			return 1;
+		}
+		token += line[i++];
+		token += line[i++];
+		parsed.push_back(vector<string>());
+		++j;
+	}
+	else {
+		parsed[j].push_back(token);
+		token = "";
+		token += line[i++];
+		token += line[i++];
+		parsed.push_back(vector<string>());
+		++j;
+	}
+	while (i == line.size()) {
+		more_input(line);
+		++i;
+	}
+	return 0;
+}
+
+int parse(string& line, vector< vector<string> >& parsed) {
+	parsed.push_back(vector<string>());
+	int i = 0;
+	int j = 0;
+	while (i < line.size()) {
+		while (i < line.size() && line[i] <= 32) ++i;
 		if (line[i] == '#')
-			processed += "# ";
-		else if (line[i] == '"' || line[i] == '\'') {
-			if (i == line.size() - 1)
-				return 2;
-			char delimiter = line[i++];
-			string token;
-			while (i < line.size() && line[i] != delimiter) {
-				token += line[i++];
-			}
-			if (i == line.size() && line[i-1] != delimiter)
-				return 2;
-			quoted.push_back(token);
-			processed += " ;; ";
-		}
-		else if (line[i] == ';')
-			processed += " ; ";
-		else if (line[i] == '&' && i+1 < line.size() && line[i+1] == '&') {
-			processed += " && ";
-			++i;
-		}
-		else if (line[i] == '|' && i+1 < line.size() && line[i+1] == '|') {
-			processed += " || ";
-			++i;
-		}
-		else
-			processed += line[i];
-	}
-	line = processed;
-	return 0;
-}
-
-//raw input is parsed into an vector of commands
-int parse(string& line, vector< vector<string> >& cmd) {
-	int q = 0;
-	vector<string> quoted;
-	int error = preprocess(line, quoted);
-	if (error)
-		return 2;
-	cmd.push_back(vector<string>());
-	stringstream ss(line);
-	string token;
-	int c = 0;
-	while (ss >> token) {
-		if (token == "#")
 			break;
-		else if (token == ";;") {
-			token = quoted[q++];
+		string token = "";
+		while (i < line.size() && line[i] > 32) {
+			if (line[i] == '\\') {
+				parse_backslash(line, i, token);
+				continue;
+			}
+			if (line[i] == '\'' || line[i] == '"') {
+				parse_quotes(line, parsed, i, token);
+				continue;
+			}
+			else if (line[i] == ';') {
+				int error = parse_connector1(line, parsed, i, j, token);
+				if (error) return error;
+				break;
+			}
+			else if (i < line.size() - 1 &&
+					((line[i] == '&' && line[i+1] == '&') ||
+					(line[i] == '|' && line[i+1] == '|'))) {
+				int error = parse_connector2(line, parsed, i, j, token);
+				if (error) return error;
+				break;
+			}
+			token += line[i++];
 		}
-		else if (token == ";" || token == "||" || token == "&&") {
-			if (cmd[c].empty() || (c > 0 && cmd[c].size() == 1))
-				return 1;
-			cmd.push_back(vector<string>());
-			++c;
-		}
-		cmd[c].push_back(token);
+		parsed[j].push_back(token);
 	}
 	return 0;
 }
 
-//converts vector of vector of strings to vector of array of cstrings
-vector<char**> c_compatible(const vector< vector<string> >& cmd) {
-	vector<char**> c_cmd;
-	for (size_t i = 0; i < cmd.size(); ++i) {
-		char **command = new char*[cmd[i].size()+1];
+vector<char**> c_compatible(const vector< vector<string> >& parsed) {
+	vector<char**> c_parsed;
+	for (size_t i = 0; i < parsed.size(); ++i) {
+		char **argv = new char*[parsed[i].size()+1];
 		size_t j;
-		for (j = 0; j < cmd[i].size(); ++j) {
-			command[j] = new char[cmd[i][j].size()+1];
-			memcpy(command[j], cmd[i][j].c_str(), cmd[i][j].size()+1);
+		for (j = 0; j < parsed[i].size(); ++j) {
+			argv[j] = new char[parsed[i][j].size()+1];
+			memcpy(argv[j], parsed[i][j].c_str(), parsed[i][j].size()+1);
 		}
-		command[j] = NULL;
-		c_cmd.push_back(command);
+		argv[j] = NULL;
+		c_parsed.push_back(argv);
 	}
-	return c_cmd;
+	return c_parsed;
 }
 
-//deletes c compatible array of cstrings
-void c_delete(const vector< vector<string> >& cmd, vector<char**>& c_cmd) {
-	for (size_t i = 0; i < cmd.size(); ++i) {
-		for (size_t j = 0; j <= cmd[i].size(); ++j) {
-			delete[] c_cmd[i][j];
+//an array of cstrings is necessary for execvp
+void c_delete(const vector< vector<string> >& parsed, vector<char**>& c_parsed) {
+	for (size_t i = 0; i < parsed.size(); ++i) {
+		for (size_t j = 0; j < parsed[i].size(); ++j) {
+			delete[] c_parsed[i][j];
 		}
-		delete[] c_cmd[i];
+		delete[] c_parsed[i];
 	}
 }
 
-//execute commands using fork, execvp, and waitpid
-void execute(const vector< vector<string> >& cmd) {
-	vector<char**> c_cmd = c_compatible(cmd);
+int execute(const vector< vector<string> >& parsed) {
+	vector<char**> c_parsed = c_compatible(parsed);
 	int status;
-	for (size_t i = 0; i < c_cmd.size(); ++i) {
+	for (size_t i = 0; i < c_parsed.size(); ++i) {
+		if (!c_parsed[i][0]) return 0;
+		if (strcmp(c_parsed[i][0], "exit") == 0 ||
+				(i > 0 && c_parsed[i][1] &&
+				 strcmp(c_parsed[i][1], "exit") == 0))
+			return 1;
 		int pid = fork();
 		if (pid < 0) {
 			perror("fork");
@@ -117,18 +185,18 @@ void execute(const vector< vector<string> >& cmd) {
 		}
 		else if (pid == 0) {
 			if (i == 0) {
-				execvp(c_cmd[i][0], c_cmd[i]);
+				execvp(c_parsed[i][0], c_parsed[i]);
 				perror("execvp");
 				exit(1);
 			}
 			else {
-				if (c_cmd[i][1] == NULL)
+				if (c_parsed[i][1] == NULL)
 					exit(0);
-				if (strcmp(c_cmd[i][0], "||") == 0 && status == 0)
+				if (strcmp(c_parsed[i][0], "||") == 0 && status == 0)
 					exit(0);
-				if (strcmp(c_cmd[i][0], "&&") == 0 && status != 0)
+				if (strcmp(c_parsed[i][0], "&&") == 0 && status != 0)
 					exit(0);
-				execvp(c_cmd[i][1], c_cmd[i]+1);
+				execvp(c_parsed[i][1], c_parsed[i]+1);
 				perror("execvp");
 				exit(1);
 			}
@@ -140,39 +208,36 @@ void execute(const vector< vector<string> >& cmd) {
 			}
 		}
 	}
-	c_delete(cmd, c_cmd);
+	c_delete(parsed, c_parsed);
+	return 0;
 }
 
-//prompt for input, get input, parse, execute, repeat
 int main() {
 	char login[MAX_LENGTH];
 	char hostname[MAX_LENGTH];
-	while (1) {
+	while (true) {
 		if (getlogin_r(login, MAX_LENGTH) != 0) {
 			perror("getlogin_r");
-			return -1;
+			return 1;
 		}
 		if (gethostname(hostname, MAX_LENGTH) != 0) {
 			perror("gethostname");
-			return -1;
+			return 1;
 		}
 		cout << login << "@" << hostname << "$ ";
 		string line;
-		getline(cin, line);
-		if (line == "exit")
+		if (cin.eof())
 			break;
-		vector< vector<string> > cmd;
-		int error = parse(line, cmd);
+		getline(cin, line);
+		vector< vector<string> > parsed;
+		int error = parse(line, parsed);
 		if (error) {
-			if (error == 1)
-				cerr << CONNECTOR_ERROR_MESSAGE <<  endl;
-			else if (error == 2)
-				cerr << QUOTES_ERROR_MESSAGE << endl;
-			else
-				cout << "unknown error" << endl;
 			continue;
 		}
-		execute(cmd);
+		bool exit_now = execute(parsed);
+		if (exit_now) {
+			break;
+		}
 	}
 	return 0;
 }
