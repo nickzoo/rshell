f1e49e29c66d742f4ae4ed3db284b76efa4d2ed2
Lucas Xu <lxu003@ucr.edu>
2014-11-24 09:06:09 -0800

    recursive remove directory

--- src/rm.cpp
+++ src/rm.cpp
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/dir.h>
 #include <unistd.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void rmdir_recursive(const string& dirpath) {
+	vector<string> directories;
+	DIR *dirptr = opendir(dirpath.c_str());
+	if (dirptr == NULL) {
+		perror("opendir");
+		exit(1);
+	}
+	dirent *entry;
+	while (entry = readdir(dirptr)) {
+		string entryname = entry->d_name;
+		if (entryname == "." || entryname == "..") continue;
+		string filepath = dirpath + entryname;
+		struct stat s;
+		if (stat(filepath.c_str(), &s) == -1) {
+			perror("stat");
+			exit(1);
+		}
+		if (s.st_mode & S_IFDIR) {
+			filepath += '/';
+			directories.push_back(filepath);
+		}
+		else {
+			if (unlink(filepath.c_str()) == -1) {
+				perror("unlink");
+				exit(1);
+			}
+		}
+	}
+	for (int i = 0; i < directories.size(); ++i)
+		rmdir_recursive(directories[i]);
+	if (rmdir(dirpath.c_str()) == -1) {
+		perror("rmdir");
+		exit(1);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	bool FLAGr = false;
@@ -20,10 +62,9 @@ int main(int argc, char *argv[]) {
 				printf("set -r flag to delete directories.\n");
 				continue;
 			}
-			if (rmdir(argv[i]) == -1) {
-				perror("rmdir");
-				return 1;
-			}
+			string dirpath = argv[i];
+			if (dirpath[dirpath.length()-1] != '/') dirpath += '/';
+			rmdir_recursive(dirpath);
 		}
 		else {
 			if (unlink(argv[i]) == -1) {
